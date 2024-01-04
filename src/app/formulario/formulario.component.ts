4edb2ff2bbd526a489c7d4e191bf03b11f6b570a
ArleyBT <abt.2709@gmail.com>
2024-01-03 19:12:02 -0500

    formularios#2

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  contacForm = new FormGroup({
+    nombre: new FormControl("", [Validators.required,Validators.minLength(5)]),
+    email: new FormControl("", [Validators.required,Validators.email])
+  })
 
+  get nombre(){
+    return this.contacForm.get("nombre")?.value
+  }
+
+  get email(){
+    return this.contacForm.get("email")?.value
+  }
+
+  enviar(){
+    const estadoformulario =  this.contacForm.status
+    console.log(estadoformulario, this.nombre, this.email)
+  }
 }
